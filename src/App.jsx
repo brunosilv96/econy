ab0aff833e6baa4d1e87d3171e6d098049c7435d
Bruno Silva <brunocosta231@gmail.com>
2023-06-28 21:17:45 -0300

    Navbar new change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 // pages
 import Home from "./components/pages/Home";
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,41 +1,96 @@
 import styles from "./Navbar.module.css";
-import { Dropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function Navbar() {
 	return (
 		<header>
-			<nav className="container-fluid" id={styles.nav_conainter}>
-				<div className="row justify-content-around">
-					<div className="col-sm-3">
-						<p>Econy</p>
-					</div>
-					<div className="col-sm-6">
-						<form className="d-flex">
-							<input type="search" placeholder="Pesquisar" className="form-control" />
-						</form>
-					</div>
-					<div className="col-sm-3" id={styles.nav_button}>
-						<Dropdown>
-							<Dropdown.Toggle id="dropdown-basic">Usuário</Dropdown.Toggle>
-							<Dropdown.Menu>
-								<Dropdown.Item>Perfil</Dropdown.Item>
-								<Dropdown.Item>Pedidos</Dropdown.Item>
-							</Dropdown.Menu>
-						</Dropdown>
+			<nav className="navbar sticky-top navbar-expand-sm bg-body-tertiary">
+				<div className="container-fluid">
+					<Link to="/" className="navbar-brand">
+						<img
+							src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg"
+							alt="Econy"
+							width="30"
+							height="24"
+							className="me-2"
+						/>
+						Econy
+					</Link>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse justify-content-evenly col-9" id="navbarSupportedContent">
+						<div className="col-12 col-sm-5 mt-2 mt-sm-0" id={styles.formNav}>
+							<div className="input-group flex-nowrap">
+								<span class="input-group-text" id="addon-wrapping">
+									<i className="bi bi-search"></i>
+								</span>
+								<input className="form-control" type="search" placeholder="Pesquisar" aria-label="Search" />
+							</div>
+						</div>
+						<div className="row justify-content-between mt-2 mt-sm-0" id={styles.marketIcons}>
+							<Link to="/carlist" className="btn col">
+								<i className="bi bi-cart3"></i>
+							</Link>
+							<Link to="/favorits" className="btn col">
+								<i className="bi bi-heart"></i>
+							</Link>
+						</div>
+						<li className="navbar-nav dropdown" id={styles.btnUser}>
+							<a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								Nome do Usuário
+							</a>
+							<ul className="dropdown-menu">
+								<li>
+									<p className="dropdown-item">
+										<strong>Olá Bruno</strong>
+									</p>
+								</li>
+								<li>
+									<hr className="dropdown-divider" />
+								</li>
+								<li>
+									<Link to="/perfil" className="dropdown-item">
+										Perfil
+									</Link>
+								</li>
+								<li>
+									<Link to="/pedidos" className="dropdown-item">
+										Pedidos
+									</Link>
+								</li>
+							</ul>
+						</li>
 					</div>
 				</div>
 			</nav>
-			<div className="nav justify-content-evenly" id={styles.subnav_container}>
-				<Link className="nav-link">Categoria 1</Link>
-				<Link className="nav-link">Categoria 1</Link>
-				<Link className="nav-link">Categoria 1</Link>
-				<Link className="nav-link">Categoria 1</Link>
-				<Link className="nav-link">Categoria 1</Link>
-				<Link className="nav-link">Categoria 1</Link>
-				<Link className="nav-link">Categoria 1</Link>
-				<Link className="nav-link">Categoria 1</Link>
-			</div>
+			<ul className="nav justify-content-center" id={styles.subnav}>
+				<li className="nav-item">
+					<a className="nav-link" aria-current="page" href="#">
+						Categoria 1
+					</a>
+				</li>
+				<li className="nav-item">
+					<a className="nav-link" href="#">
+						Categoria 2
+					</a>
+				</li>
+				<li className="nav-item">
+					<a className="nav-link" href="#">
+						Categoria 3
+					</a>
+				</li>
+				<li className="nav-item">
+					<a className="nav-link">Categoria 4</a>
+				</li>
+			</ul>
 		</header>
 	);
 }
